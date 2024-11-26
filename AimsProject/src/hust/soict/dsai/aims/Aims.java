0a623052f66bf3d6c23af12a69f224ccdd54ad3a
Hoàng Thái Dương_20225821 <thaiduong040404@gmail.com>
2024-11-26 21:58:38 +0700

    bai8

---
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -0,0 +1,33 @@
+package hust.soict.dsai.aims;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Aims {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "Unknown", 90, 18.99f);
+
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+
+        // Print the cart contents
+        cart.printCart();
+
+        // Search by ID
+        System.out.println("\nSearch by ID (2):");
+        cart.searchById(2);
+
+        // Search by Title
+        System.out.println("\nSearch by Title ('Star'):");
+        cart.searchByTitle("Star");
+
+        // Remove a DVD and print the updated cart
+        cart.removeDigitalVideoDisc(dvd2);
+        System.out.println("\nAfter removing 'Star Wars':");
+        cart.printCart();
+    }
+}
---
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -0,0 +1,53 @@
+package hust.soict.dsai.aims.store;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Store {
+    public static final int MAX_ITEMS = 50;
+    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_ITEMS];
+    private int qtyInStore = 0;
+
+    // Method to add a DVD to the store
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (qtyInStore >= MAX_ITEMS) {
+            System.out.println("The store is full. Cannot add more DVDs.");
+        } else {
+            itemsInStore[qtyInStore] = dvd;
+            qtyInStore++;
+            System.out.println("DVD added to store: " + dvd.toString());
+        }
+    }
+
+    // Method to remove a DVD from the store
+    public void removeDVD(DigitalVideoDisc dvd) {
+        int pos = -1;
+        for (int i = 0; i < qtyInStore; i++) {
+            if (itemsInStore[i] == dvd) {
+                pos = i;
+                break;
+            }
+        }
+
+        if (pos < 0) {
+            System.out.println("DVD not found in store: " + dvd.toString());
+        } else {
+            for (int i = pos; i < qtyInStore - 1; i++) {
+                itemsInStore[i] = itemsInStore[i + 1];
+            }
+            qtyInStore--;
+            System.out.println("DVD removed from store: " + dvd.toString());
+        }
+    }
+
+    // Method to display all DVDs in the store
+    public void displayStore() {
+        System.out.println("********************STORE********************");
+        if (qtyInStore == 0) {
+            System.out.println("The store is empty.");
+        } else {
+            for (int i = 0; i < qtyInStore; i++) {
+                System.out.println((i + 1) + ". " + itemsInStore[i].toString());
+            }
+        }
+        System.out.println("********************************************");
+    }
+}
